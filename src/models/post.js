722c3cde1d7b99da9751cb76b881548ff2783da0
paragao01 <paragao01@gmail.com>
2024-12-17 20:32:45 -0300

    Primeiro Commit

--- src/models/post.js
+++ src/models/post.js
@@ -5,9 +5,9 @@ const strToBool = (value) => {
   return value === 'true';
 };
 
-const DB_DATABASE = process.env.DB_DATABASE || "kubedevnews";
-const DB_USERNAME = process.env.DB_USERNAME || "kubedevnews";
-const DB_PASSWORD = process.env.DB_PASSWORD || "Pg#123";
+const DB_DATABASE = process.env.DB_DATABASE || "kubenews";
+const DB_USERNAME = process.env.DB_USERNAME || "kubenews";
+const DB_PASSWORD = process.env.DB_PASSWORD || "Pg123";
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PORT = parseInt(process.env.DB_PORT, 10) || 5432; 
 const DB_SSL_REQUIRE =  strToBool(process.env.DB_SSL_REQUIRE) || false;
